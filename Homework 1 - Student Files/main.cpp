94f46214af890869b54427b35c95a8bde3ddeb30
Dino1 <brandoncloutier412@gmail.com>
2024-02-09 17:16:44 -0800

    s

--- Homework 1 - Student Files/main.cpp
+++ Homework 1 - Student Files/main.cpp
@@ -10,21 +10,24 @@ int main() {
             << "   enclose string entries in quotes, separate fields with comas\n"
             << "   for example:  \" 00016000306707 \", \" Betty Crocker \", \" Betty Crocker Double Chocolate Chunk Cookie Mix \", 17.19\n"
             << "   Enter CTL-Z (Windows) or CTL-D (Linux) to quit\n";
-  std::cout << "\n";
+  std::cout << '\n';
 
   // Vector of grocery items
   std::vector<std::unique_ptr<GroceryItem>> groceryItems{};
 
   // Populate groceryItems vector
-  while( !std::cin.eof() )
+  bool continueLoop = true;
+  while( continueLoop )
   {
     std::unique_ptr<GroceryItem> newItem = std::make_unique<GroceryItem>();
 
     std::cout << "Enter UPC, Product Brand, Product Name, and Price\n";
     std::cout << "Item added to shopping cart: ";
 
-    std::cin >> *newItem;
-    std::cout << "\n";
+    if ( !(std::cin >> *newItem)) {
+      continueLoop = false;
+    }
+    std::cout << '\n';
 
     if( std::cin.good() ) {
       groceryItems.push_back( std::move( newItem ) );
@@ -32,13 +35,13 @@ int main() {
   }
 
 
-  std::cout << "\n";
+  std::cout << '\n';
   // Print out the groceryItems vector in reverse
   std::cout << "Here is an itemized list of the items in your shopping cart:\n";
 
   for( auto it = groceryItems.rbegin(); it != groceryItems.rend(); ++it)
   {
-    std::cout << *(*it) << "\n";
+    std::cout << *(*it) << '\n';
   }
 
   return 0;
